f2160b61a67aa6346a9631c9d15e45709c669d6e
eoftt1gr <119725650+eoftt1gr@users.noreply.github.com>
2022-12-05 13:27:13 +0300

    Add files via upload

---
+++ load.c
@@ -0,0 +1,22 @@
+#include "load.h"
+#include "fun.h"
+#include <stdio.h>
+#include <dlfcn.h>
+void LoadRun(const char * const s) {
+void * lib;
+void (*fun)(void);
+void (*fun1)(void);
+lib = dlopen(s_RTLD_LAZY);
+if (!lib) {
+printf("cannot open library '%s'\n", s);
+return;
+}
+fun = (void (*)(void))dlsym(lib, "obrMas");
+if (fun != NULL)
+fun();
+fun1 = (void (*)(void))dlsym(lib, "obrMat");
+if (fun1 != NULL)
+fun1();
+dlcose(lib);
+}
+
